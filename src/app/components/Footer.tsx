31b308366d15273fc3ca28fd613f6bf054a1df3c
Hitendra <hitendrasinghchoudhary987@gmail.com>
2024-07-04 20:24:12 +0530

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
             {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title} key={columns.label}>
+              <FooterColumn title={columns.title} key={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {columns.links.map((link) => (
                     <Link href="/" key={link}>
@@ -67,11 +67,10 @@ const Footer = () => {
 
 type FooterColumnProps = {
   title: string;
-  label: string; // Add the 'label' property
   children: React.ReactNode;
 }
 
-const FooterColumn = ({ title, label, children }: FooterColumnProps) => {
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className="flex flex-col gap-5">
       <h4 className="bold-18 whitespace-nowrap">{title}</h4>
